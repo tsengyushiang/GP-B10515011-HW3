44f4130261b8ea3f76abc8d5ff8f86b81558c67f
tsengyushiang <tsengyushiang@gmail.com>
2019-05-09 03:07:49 +0800

    多點觸控測試

---
+++ Assets/script/move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+    public float threshold;
+    void Awake() {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+    void Update()
+    {
+        float deltaY = Input.acceleration.y + 0.5f;
+
+        if (transform.localPosition.y + deltaY > threshold) return;
+        if (transform.localPosition.y + deltaY < -threshold) return;
+
+        //transform.Translate(0, deltaY, 0);
+    }
+}
---
+++ Assets/script/touch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+    public GameObject bulletPool;
+    public GameObject bullet;
+    List<Vector3> touches= new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches) {
+            Vector3 touchPoint = Camera.main.ScreenToWorldPoint(touch.position);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (touches.Count < touch.fingerId)
+                {
+                    touches.Insert(touch.fingerId, touchPoint);
+                }
+                touches[touch.fingerId] = touchPoint;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                //touches.Insert(touch.fingerId, touchPoint);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                //touches.Insert(touch.fingerId);
+            }
+        }
+        Debug.Log(touches.Count);
+        /*
+        foreach (Vector3 v in touches) {
+
+            GameObject newbullet =Instantiate(bullet);
+            newbullet.transform.position = v;
+        }
+        */
+    }
+}
